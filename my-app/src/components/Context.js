9b875666a4c5702f93bc966bd02f1585995e7ff6
YroslavOne <berilloyaroslav@gmail.com>
2024-02-27 17:43:28 +0700

    17.43 27.02.2024

--- my-app/src/components/Context.js
+++ my-app/src/components/Context.js
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
+import { ChatRight, Kanban, Motherboard, Telephone } from "react-bootstrap-icons";
 
 export const Context = React.createContext({
   selectedFirst: "X",
   setSelectedFirst: () => {},
 });
 
-const
-
 export const ContextProvider = ({ children }) => {
+  const meetingItems = [
+    {
+      icon: <Kanban/>,
+      name: "Project meeting",
+      className: "red",
+    },
+    {
+      icon: <ChatRight/>,
+      name: "Meeting",
+      className: "red",
+    },
+    {
+      icon: <Telephone/>,
+      name: "Call",
+      className: "red",
+    },
+    {
+      icon: <Motherboard/>,
+      name: "Other",
+      className: "red",
+    },
+  ];
 
   const [selectedFirst, setSelectedFirst] = useState("X");
-
 
   return (
     <Context.Provider
       value={{
         selectedFirst,
-        setSelectedFirst
+        setSelectedFirst,
+        meetingItems
       }}
     >
       {children}
--- my-app/src/components/appComponents/mainComponents/CreateTask/CreateTask.js
+++ my-app/src/components/appComponents/mainComponents/CreateTask/CreateTask.js
@@ -1,9 +1,14 @@
+import MeetingType from "./TaskItems/MeetingType";
+import {Kanban} from "react-bootstrap-icons"
+import MyApp from "./TaskItems/TimerPicker";
+
 function CreateTask(){
     return(
         <div>
             <input
             placeholder="name task"></input>
-            
+            <MeetingType/>
+            <MyApp/>
         </div>
     )
 }
